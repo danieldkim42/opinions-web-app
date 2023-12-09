44c7bc3ca8bbe8d2fc8db81348e31a1814f9236c
Daniel Kim <danieldkim.42@gmail.com>
2023-12-09 09:17:49 -0800

    set up with nav bar and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import NavBar from './containers/NavBar';
+import Home from './pages/Home';
+import Search from './pages/Search';
+
 
 function App() {
   return (
-    <div>
-      <h1>Hello, React!</h1>
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/search' element={<Search />} />
+      </Routes>
+      
+    </>
   );
 }
 
