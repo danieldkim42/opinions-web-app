a27aa0df9c9ae966915f96c12dc1d4f425564798
Daniel Kim <danieldkim.42@gmail.com>
2023-12-09 09:20:29 -0800

    displays question

---
+++ src/containers/TopContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TopContainer() {
+  return (
+    <div className="top-container">
+      What are you thinking?
+    </div>
+  );
+}
+
+export default TopContainer;
