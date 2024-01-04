07997c8ce259aeb4b138b76163b4aa5a81eb8595
Daniel Kim <danieldkim.42@gmail.com>
2024-01-04 07:24:37 -0800

    added getEntries

--- server/controllers/dbController.js
+++ server/controllers/dbController.js
@@ -23,14 +23,34 @@ dbController.addEntry = (req, res, next) => {
  * Retrieves text from the database.
  */
 dbController.getEntries = (req, res, next) => {
-  Comment.find({ "keywords.propertyName": "propertyValue" })
+  // Get the array of keywords from res.locals.result
+  const keywords = res.locals.result;
+
+  // Initialize the query object
+  let query = {};
+
+  // Construct the query to find documents where all specified keywords exist
+  for (const keyword of keywords) {
+    query[`keywords.${keyword}`] = { $exists: true };
+  }
+
+  // Use the Comment model to find entries in the database that match the query
+  Comment.find(query)
     .then((data) => {
+      // If successful, store the retrieved data in res.locals.text
       res.locals.text = data;
+
+      // Log the retrieved data to the console
+      console.log('text: ', res.locals.text);
+
+      // Call the next middleware or route handler in the chain
       return next();
     })
     .catch((error) => {
+      // If an error occurs during the database query, pass it to the error handling middleware
       return next(error);
     });
-}
+};
+
 
 module.exports = dbController;
